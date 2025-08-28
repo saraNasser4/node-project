75e4fe0c9a99a4b95cccc12a08d5eabc5918c522
saraNasser4 <saransser6@gmail.com>
2025-08-28 11:25:08 +0300

    [12] working with the methods and urls to put the rules here!

--- server.js
+++ server.js
@@ -11,10 +11,33 @@ const server = http.createServer((req,res)=>{
     //res.end(JSON.stringify({ message: 'Server Error' }));
     
     console.log(`url: ${req.url}`);
-    console.log(`method: ${req.method}`);
+    console.log(`method: ${req.method}`);  //postman will help
     
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end("<h1>Hello World</h1>");
+    //res.writeHead(200, { 'Content-Type': 'text/html'});
+    //res.end("<h1>Hello World</h1>");
+
+    try{
+        if(res.method === "GET") {
+
+            if(res.url === "/") {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>Hello World!</h1>");
+            
+            } else if(res.url === "/about") {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>About the World!</h1>");
+
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end("<h1>Not Found</h1>");
+            }
+        } else {
+            throw new Error("Method not Allowed");
+        }
+    }catch {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });   //500--> server error
+        res.end("Server Error");
+    }
 })
 
 server.listen(PORT, ()=> {
