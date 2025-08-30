b66ab359a252842daccf405716bdf23e631d7f2e
saraNasser4 <saransser6@gmail.com>
2025-08-30 18:24:47 +0300

    [13] link html page with node.js instead of writing them in the js file

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 //4
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req,res)=>{
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req,res)=>{
     //res.setHeader('Content-Type', 'text/html');  // or text/plain
     //res.statusCode = 404;
     //res.end("<h1>Hello World</h1>")
@@ -17,20 +24,31 @@ const server = http.createServer((req,res)=>{
     //res.end("<h1>Hello World</h1>");
 
     try{
-        if(res.method === "GET") {
+        if(req.method === "GET") {
+            let filePath;
 
-            if(res.url === "/") {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end("<h1>Hello World!</h1>");
-            
-            } else if(res.url === "/about") {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end("<h1>About the World!</h1>");
+            if(req.url === "/") {
+                //res.writeHead(200, { 'Content-Type': 'text/html' });
+                //res.end("<h1>Hello World!</h1>");
+                
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if(req.url === "/about") {
+                //res.writeHead(200, { 'Content-Type': 'text/html' });
+                //res.end("<h1>About the World!</h1>");
+
+                filePath = path.join(__dirname, 'public', 'about.html');
 
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end("<h1>Not Found</h1>");
+                //res.writeHead(404, { 'Content-Type': 'text/html' });
+                //res.end("<h1>Not Found</h1>");
+
+                throw new Error("Not Found");
             }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Type-Content', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error("Method not Allowed");
         }
