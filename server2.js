70c400f3b17c6f45ec73ebbfdd5dd6b2c49643e2
saraNasser4 <saransser6@gmail.com>
2025-09-01 12:21:16 +0300

    [14] creating API to check about the users

---
+++ server2.js
@@ -0,0 +1,33 @@
+import { createServer } from 'http';
+
+const PORT = process.env.PORT;
+
+const users = [
+    { id: 1, name: "Sara Nasser" },
+    { id: 2, name: "johnny Deep" },
+    { id: 3, name: "Micheal Jackson" },
+    { id: 4, name: "Alon Musk" },
+];
+
+const server = createServer((req, res)=> {
+    if(req.url === "/api/users" && req.method === "GET") {
+        res.setHeader('Type-Contact', 'application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+
+    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+        const id = req.url.split('/')[3];
+        console.log(id)
+        res.setHeader('Type-Contact', 'application/json');
+        res.write(JSON.stringify(users[id -1]));
+        res.end();
+        
+    } else {
+        res.setHeader('Type-Contact', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({ massage: 'Route Not Fount' })); 
+        res.end();
+    }
+});
+
+server.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
