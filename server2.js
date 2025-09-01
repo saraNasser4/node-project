277e77b64d4148a1891ca0b94eb0d39d54c2d47f
saraNasser4 <saransser6@gmail.com>
2025-09-01 12:29:19 +0300

    [15] adding middleware

--- server2.js
+++ server2.js
@@ -9,25 +9,33 @@ const users = [
     { id: 4, name: "Alon Musk" },
 ];
 
-const server = createServer((req, res)=> {
-    if(req.url === "/api/users" && req.method === "GET") {
-        res.setHeader('Type-Contact', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
+// logger middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
 
-    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-        const id = req.url.split('/')[3];
-        console.log(id)
-        res.setHeader('Type-Contact', 'application/json');
-        res.write(JSON.stringify(users[id -1]));
-        res.end();
-        
-    } else {
-        res.setHeader('Type-Contact', 'application/json');
-        res.statusCode = 404;
-        res.write(JSON.stringify({ massage: 'Route Not Fount' })); 
-        res.end();
-    }
+const server = createServer((req, res)=> {
+    logger(req, res, ()=> {
+        if(req.url === "/api/users" && req.method === "GET") {
+            res.setHeader('Type-Contact', 'application/json');
+            res.write(JSON.stringify(users));
+            res.end();
+    
+        } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+            const id = req.url.split('/')[3];
+            console.log(id)
+            res.setHeader('Type-Contact', 'application/json');
+            res.write(JSON.stringify(users[id -1]));
+            res.end();
+            
+        } else {
+            res.setHeader('Type-Contact', 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({ massage: 'Route Not Fount' })); 
+            res.end();
+        }
+    });
 });
 
 server.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
