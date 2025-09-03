bb2987f656622015e8995ab0ecffa7fbfd5b0fb0
saraNasser4 <saransser6@gmail.com>
2025-09-03 11:40:23 +0300

    [16] make the code more pretty ;(

---
+++ loggerMiddleware.js
@@ -0,0 +1,5 @@
+// logger middleware
+export default function logger (req, res, next) {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
--- server2.js
+++ server2.js
@@ -1,4 +1,5 @@
 import { createServer } from 'http';
+import logger from './loggerMiddleware.js';
 
 const PORT = process.env.PORT;
 
@@ -9,33 +10,36 @@ const users = [
     { id: 4, name: "Alon Musk" },
 ];
 
-// logger middleware
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
+console.log()
 const server = createServer((req, res)=> {
     logger(req, res, ()=> {
         if(req.url === "/api/users" && req.method === "GET") {
-            res.setHeader('Type-Contact', 'application/json');
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 200;
             res.write(JSON.stringify(users));
             res.end();
     
         } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
             const id = req.url.split('/')[3];
-            console.log(id)
-            res.setHeader('Type-Contact', 'application/json');
-            res.write(JSON.stringify(users[id -1]));
+            const user = users.find(u => u.id === parseInt(id));
+            res.setHeader('Content-Type', 'application/json');
+
+            if(user) {
+                res.statusCode = 200;
+                res.write(JSON.stringify(user));
+            } else {
+                res.statusCode = 404;
+                res.write(JSON.stringify({ message: 'User Not Found' }))
+            }
             res.end();
             
         } else {
-            res.setHeader('Type-Contact', 'application/json');
+            res.setHeader('Content-Type', 'application/json');
             res.statusCode = 404;
-            res.write(JSON.stringify({ massage: 'Route Not Fount' })); 
+            res.write(JSON.stringify({ massage: 'Route Not Found' })); 
             res.end();
         }
     });
 });
 
-server.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
+server.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
